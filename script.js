f2ed3e1e71e6a71314a8f02444de0bab75e85287
Zaina <zeinaeabdelfattah@gmail.com>
2025-10-06 09:00:15 +0400

    applying some discord feedback

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 const container = document.querySelector ('.container');
 
+function randomColor(event) {
+    const firstColor = Math.floor (Math.random()* 256);
+    const secondColor = Math.floor (Math.random()* 256);
+    const thirdColor = Math.floor (Math.random()* 256);
+    event.target.style.background = `rgb(${firstColor}, ${secondColor}, ${thirdColor})`;
+}
+
 for (let i = 0; i < 256; i++) {
     const square = document.createElement ("div");
     square.classList.add("square");
-    square.addEventListener("mouseover", event => {
-        const firstColor = Math.floor (Math.random()* 256);
-        const secondColor = Math.floor (Math.random()* 256);
-        const thirdColor = Math.floor (Math.random()* 256);
-        event.target.style.background = `rgb(${firstColor}, ${secondColor}, ${thirdColor})`;
-    }); 
+    square.addEventListener("mouseover", randomColor); 
     container.appendChild(square);
 }
 
-const newGrid = document.getElementById ("new");
-newGrid.addEventListener("click", () => {
+// useful feedback that i will not apply this time but will want to find later:
+// Consider using ‘event delegation’ to target the squares instead of adding them in a loop. 
+// It’s fine for a low number like ‘256’, but when you are adding event listeners to 
+// higher numbers like ‘100 × 100’ squares - that might make the browser sluggish.
+
+const newGridButton = document.getElementById ("new");
+newGridButton.addEventListener("click", () => {
     const promptUser = prompt ("Select a new grid size between 1 and 100");
     const num = Number(promptUser);
     if (promptUser === null) {
@@ -27,12 +34,7 @@ newGrid.addEventListener("click", () => {
             const newSquareSize = 480 / num;
             newSquare.style.width = `${newSquareSize}px`;
             newSquare.style.height = `${newSquareSize}px`;
-            newSquare.addEventListener("mouseover", event => {
-                const firstColor = Math.floor (Math.random()* 256);
-                const secondColor = Math.floor (Math.random()* 256);
-                const thirdColor = Math.floor (Math.random()* 256);
-                event.target.style.background = `rgb(${firstColor}, ${secondColor}, ${thirdColor})`;
-            });
+            newSquare.addEventListener("mouseover", randomColor);
             container.appendChild(newSquare);
         }
     } else {
